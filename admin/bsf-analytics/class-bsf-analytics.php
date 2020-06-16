99298afb5764c8318ef3c8d7660c6fb6eac30eb5
hemant7405 <hdkothari81@gmail.com>
2020-06-16 16:17:10 +0530

    updated i18n

--- admin/bsf-analytics/class-bsf-analytics.php
+++ admin/bsf-analytics/class-bsf-analytics.php
@@ -180,10 +180,10 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 			}
 
 			/* translators: %s product name */
-			$notice_string = __( 'Want to help make <strong>%1s</strong> even more awesome? Allow us to collect non-sensitive diagnostic data and usage information. ' );
+			$notice_string = __( 'Want to help make <strong>%1s</strong> even more awesome? Allow us to collect non-sensitive diagnostic data and usage information. ', 'all-in-one-schemaorg-rich-snippets' );
 
 			if ( is_multisite() ) {
-				$notice_string .= __( 'This will be applicable for all sites from the network.' );
+				$notice_string .= __( 'This will be applicable for all sites from the network.', 'all-in-one-schemaorg-rich-snippets' );
 			}
 
 			$language_dir = is_rtl() ? 'rtl' : 'ltr';
@@ -207,14 +207,14 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 								</div>
 							</div>',
 						/* translators: %s usage doc link */
-						sprintf( $notice_string . '<span dir="%2s"><a href="%3s" target="_blank" rel="noreferrer noopener">%4s</a><span>', esc_html( $this->get_product_name() ), $language_dir, esc_url( $this->usage_doc_link ), __( ' Know More.', 'astra' ) ),
+						sprintf( $notice_string . '<span dir="%2s"><a href="%3s" target="_blank" rel="noreferrer noopener">%4s</a><span>', esc_html( $this->get_product_name() ), $language_dir, esc_url( $this->usage_doc_link ), __( ' Know More.', 'all-in-one-schemaorg-rich-snippets' ) ),
 						add_query_arg(
 							array(
 								'bsf_analytics_optin' => 'yes',
 								'bsf_analytics_nonce' => wp_create_nonce( 'bsf_analytics_optin' ),
 							)
 						),
-						__( 'Yes! Allow it' ),
+						__( 'Yes! Allow it', 'all-in-one-schemaorg-rich-snippets' ),
 						add_query_arg(
 							array(
 								'bsf_analytics_optin' => 'no',
@@ -222,7 +222,7 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 							)
 						),
 						MONTH_IN_SECONDS,
-						__( 'No Thanks' )
+						__( 'No Thanks', 'all-in-one-schemaorg-rich-snippets' )
 					),
 					'show_if'                    => true,
 					'repeat-notice-after'        => false,
@@ -291,7 +291,7 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 		public function every_two_days_schedule( $schedules ) {
 			$schedules['every_two_days'] = array(
 				'interval' => 2 * DAY_IN_SECONDS,
-				'display'  => __( 'Every two days' ),
+				'display'  => __( 'Every two days', 'all-in-one-schemaorg-rich-snippets' ),
 			);
 
 			return $schedules;
@@ -345,7 +345,7 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 
 			add_settings_field(
 				'bsf-analytics-optin',       // Field ID.
-				__( 'Usage Tracking' ),       // Field title.
+				__( 'Usage Tracking', 'all-in-one-schemaorg-rich-snippets' ),       // Field title.
 				array( $this, 'render_settings_field_html' ), // Field callback function.
 				'general'                    // Settings page slug.
 			);
@@ -377,15 +377,15 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 			<label for="bsf-analytics-optin">
 				<input id="bsf-analytics-optin" type="checkbox" value="1" name="bsf_analytics_optin" <?php checked( get_site_option( 'bsf_analytics_optin', 'no' ), 'yes' ); ?>>
 				<?php
-				esc_html_e( 'Allow Brainstorm Force products to track non-sensitive usage tracking data.' );
+				esc_html_e( 'Allow Brainstorm Force products to track non-sensitive usage tracking data.', 'all-in-one-schemaorg-rich-snippets' );
 
 				if ( is_multisite() ) {
-					esc_html_e( ' This will be applicable for all sites from the network.' );
+					esc_html_e( ' This will be applicable for all sites from the network.', 'all-in-one-schemaorg-rich-snippets' );
 				}
 				?>
 			</label>
 			<?php
-			echo wp_kses_post( sprintf( '<a href="%1s" target="_blank" rel="noreferrer noopener">%2s</a>', esc_url( $this->usage_doc_link ), __( 'Learn More.' ) ) );
+			echo wp_kses_post( sprintf( '<a href="%1s" target="_blank" rel="noreferrer noopener">%2s</a>', esc_url( $this->usage_doc_link ), __( 'Learn More.', 'all-in-one-schemaorg-rich-snippets' ) ) );
 			?>
 			</fieldset>
 			<?php
