2fc28210304aa843b515f5f212a4c5e6530d81e7
hemant7405 <hdkothari81@gmail.com>
2020-08-18 16:44:59 +0530

    updated the bsf-anayltics code

--- index.php
+++ index.php
@@ -374,7 +374,23 @@ if ( is_admin() ) {
 	// Load Astra Notices library.
 	require_once plugin_dir_path( __FILE__ ) . '/lib/notices/class-astra-notices.php';
 }
-	require_once plugin_dir_path( __FILE__ ) . 'admin/bsf-analytics/class-bsf-analytics.php';
+			// BSF Analytics library.
+			if ( ! class_exists( 'BSF_Analytics_Loader' ) ) {
+				require_once plugin_dir_path( __FILE__ ) . 'admin/bsf-analytics/class-bsf-analytics-loader.php';
+			}
+
+			$bsf_analytics = BSF_Analytics_Loader::get_instance();
+
+			$bsf_analytics->set_entity(
+				array(
+					'bsf' => array(
+						'product_name'    => 'All In One Schema Rich Snippets',
+						'path'            => plugin_dir_path( __FILE__ ) . 'admin/bsf-analytics',
+						'author'          => 'Brainstorm Force',
+						'time_to_display' => '+24 hours',
+					),
+				)
+			);
 	add_filter( 'bsf_meta_boxes', 'bsf_metaboxes' );
 // Instantiating the Class.
 if ( class_exists( 'RichSnippets' ) ) {
