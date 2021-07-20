8ce092ad9ccbda75f39d1757af2beb931440587a
Gaurav Khupse <gauravk@bsf.io>
2021-07-20 15:49:39 +0530

    Updated Text domain

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * Author URI: https://www.brainstormforce.com
  * Description: Welcome to the Schema - All In One Schema Rich Snippets! You can now easily add schema markup on various * pages and posts of your website. Implement schema types such as Review, Events, Recipes, Article, Products, Services * *etc.
  * Version: 1.6.4
- * Text Domain: rich-snippets
+ * Text Domain: all-in-one-schemaorg-rich-snippets
  * License: GPL2
  *
  * @package AIOSRS.
