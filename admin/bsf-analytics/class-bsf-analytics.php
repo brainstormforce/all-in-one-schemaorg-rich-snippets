72e3defed4eac721a594d9ec3bb6710524d8dc1f
Abhishek Bhikule <abhishekbhikule76@gmail.com>
2025-02-24 15:47:19 +0530

    updated bsf analytics package

--- admin/bsf-analytics/class-bsf-analytics.php
+++ admin/bsf-analytics/class-bsf-analytics.php
@@ -53,7 +53,7 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 			define( 'BSF_ANALYTICS_URI', $this->get_analytics_url( $analytics_path ) );
 
 			add_action( 'admin_init', array( $this, 'handle_optin_optout' ) );
-			add_action( 'admin_notices', array( $this, 'option_notice' ) );
+			add_action( 'admin_init', array( $this, 'option_notice' ) );
 			add_action( 'init', array( $this, 'maybe_track_analytics' ), 99 );
 
 			$this->set_actions();
@@ -61,6 +61,25 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 			add_action( 'admin_init', array( $this, 'register_usage_tracking_setting' ) );
 
 			$this->includes();
+
+			$this->load_deactivation_survey_actions();
+		}
+
+		/**
+		 * Function to load the deactivation survey form actions.
+		 *
+		 * @since 1.1.6
+		 * @return void
+		 */
+		public function load_deactivation_survey_actions() {
+
+			// If not in a admin area then return it.
+			if ( ! is_admin() ) {
+				return;
+			}
+
+			add_filter( 'uds_survey_vars', array( $this, 'add_slugs_to_uds_vars' ) );
+			add_action( 'admin_footer', array( $this, 'load_deactivation_survey_form' ) );
 		}
 
 		/**
@@ -85,17 +104,12 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 		 * @since 1.0.0
 		 */
 		public function get_analytics_url( $analytics_path ) {
-			return str_replace( WP_CONTENT_DIR, content_url(), $analytics_path );
-		}
 
-		/**
-		 * Get API URL for sending analytics.
-		 *
-		 * @return string API URL.
-		 * @since 1.0.0
-		 */
-		private function get_api_url() {
-			return defined( 'BSF_API_URL' ) ? BSF_API_URL : 'https://support.brainstormforce.com/';
+			$content_dir_path = wp_normalize_path( WP_CONTENT_DIR );
+
+			$analytics_path = wp_normalize_path( $analytics_path );
+
+			return str_replace( $content_dir_path, content_url(), $analytics_path );
 		}
 
 		/**
@@ -115,9 +129,9 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 			$file_rtl = ( is_rtl() ) ? '-rtl' : '';
 			$css_ext  = ( SCRIPT_DEBUG ) ? '.css' : '.min.css';
 
-			$css_uri = ( defined( 'BSF_ANALYTICS_URI' ) ? BSF_ANALYTICS_URI : '' ) . '/assets/css/' . $dir_name . '/style' . $file_rtl . $css_ext;
+			$css_uri = BSF_ANALYTICS_URI . '/assets/css/' . $dir_name . '/style' . $file_rtl . $css_ext;
 
-			wp_enqueue_style( 'bsf-analytics-admin-style', $css_uri, false, ( defined( 'BSF_ANALYTICS_VERSION' ) ? BSF_ANALYTICS_VERSION : '' ), 'all' );
+			wp_enqueue_style( 'bsf-analytics-admin-style', $css_uri, false, BSF_ANALYTICS_VERSION, 'all' );
 		}
 
 		/**
@@ -126,8 +140,11 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 		 * @since 1.0.0
 		 */
 		public function send() {
+
+			$api_url = BSF_Analytics_Helper::get_api_url();
+
 			wp_remote_post(
-				$this->get_api_url() . 'wp-json/bsf-core/v1/analytics/',
+				$api_url . 'api/analytics/',
 				array(
 					'body'     => BSF_Analytics_Stats::instance()->get_stats(),
 					'timeout'  => 5,
@@ -208,10 +225,10 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 				}
 
 				/* translators: %s product name */
-				$notice_string = __( 'Want to help make <strong>%1s</strong> even more awesome? Allow us to collect non-sensitive diagnostic data and usage information. ', 'all-in-one-schemaorg-rich-snippets' );
+				$notice_string = sprintf( __( 'Want to help make %1s even more awesome? Allow us to collect non-sensitive diagnostic data and usage information. ' ), '<strong>' . esc_html( $data['product_name'] ) . '</strong>' );
 
 				if ( is_multisite() ) {
-					$notice_string .= __( 'This will be applicable for all sites from the network.', 'all-in-one-schemaorg-rich-snippets' );
+					$notice_string .= __( 'This will be applicable for all sites from the network.' );
 				}
 
 				$language_dir = is_rtl() ? 'rtl' : 'ltr';
@@ -235,24 +252,28 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 									</div>
 								</div>',
 							/* translators: %s usage doc link */
-							sprintf( $notice_string . '<span dir="%2s"><a href="%3s" target="_blank" rel="noreferrer noopener">%4s</a><span>', esc_html( $data['product_name'] ), $language_dir, esc_url( $usage_doc_link ), __( ' Know More.', 'all-in-one-schemaorg-rich-snippets' ) ),
-							add_query_arg(
-								array(
-									$key . '_analytics_optin' => 'yes',
-									$key . '_analytics_nonce' => wp_create_nonce( $key . '_analytics_optin' ),
-									'bsf_analytics_source' => $key,
+							sprintf( $notice_string . '<span dir="%1s"><a href="%2s" target="_blank" rel="noreferrer noopener">%3s</a><span>', $language_dir, esc_url( $usage_doc_link ), __( ' Know More.' ) ),
+							esc_url(
+								add_query_arg(
+									array(
+										$key . '_analytics_optin' => 'yes',
+										$key . '_analytics_nonce' => wp_create_nonce( $key . '_analytics_optin' ),
+										'bsf_analytics_source' => $key,
+									)
 								)
 							),
-							__( 'Yes! Allow it', 'all-in-one-schemaorg-rich-snippets' ),
-							add_query_arg(
-								array(
-									$key . '_analytics_optin' => 'no',
-									$key . '_analytics_nonce' => wp_create_nonce( $key . '_analytics_optin' ),
-									'bsf_analytics_source' => $key,
+							__( 'Yes! Allow it' ),
+							esc_url(
+								add_query_arg(
+									array(
+										$key . '_analytics_optin' => 'no',
+										$key . '_analytics_nonce' => wp_create_nonce( $key . '_analytics_optin' ),
+										'bsf_analytics_source' => $key,
+									)
 								)
 							),
 							MONTH_IN_SECONDS,
-							__( 'No Thanks', 'all-in-one-schemaorg-rich-snippets' )
+							__( 'No Thanks' )
 						),
 						'show_if'                    => true,
 						'repeat-notice-after'        => false,
@@ -293,11 +314,13 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 			}
 
 			wp_safe_redirect(
-				remove_query_arg(
-					array(
-						$source . '_analytics_optin',
-						$source . '_analytics_nonce',
-						'bsf_analytics_source',
+				esc_url_raw(
+					remove_query_arg(
+						array(
+							$source . '_analytics_optin',
+							$source . '_analytics_nonce',
+							'bsf_analytics_source',
+						)
 					)
 				)
 			);
@@ -329,7 +352,12 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 		 * @since 1.0.0
 		 */
 		private function includes() {
+			require_once __DIR__ . '/classes/class-bsf-analytics-helper.php';
 			require_once __DIR__ . '/class-bsf-analytics-stats.php';
+
+			// Loads all the modules.
+			require_once __DIR__ . '/modules/deactivation-survey/classes/class-deactivation-survey-feedback.php';
+			require_once __DIR__ . '/modules/utm-analytics.php';
 		}
 
 		/**
@@ -356,7 +384,7 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 
 				add_settings_field(
 					$key . '-analytics-optin',       // Field ID.
-					__( 'Usage Tracking', 'all-in-one-schemaorg-rich-snippets' ),       // Field title.
+					__( 'Usage Tracking' ),       // Field title.
 					array( $this, 'render_settings_field_html' ), // Field callback function.
 					'general',
 					'default',                   // Settings page slug.
@@ -400,15 +428,15 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 				<input id="<?php echo esc_attr( $args['id'] ); ?>" type="checkbox" value="1" name="<?php echo esc_attr( $args['name'] ); ?>" <?php checked( get_site_option( $args['name'], 'no' ), 'yes' ); ?>>
 				<?php
 				/* translators: %s Product title */
-				echo esc_html( sprintf( __( 'Allow %s products to track non-sensitive usage tracking data.', 'all-in-one-schemaorg-rich-snippets' ), $args['title'] ) );// phpcs:ignore WordPress.WP.I18n.NonSingularStringLiteralText
+				echo esc_html( sprintf( __( 'Allow %s products to track non-sensitive usage tracking data.' ), $args['title'] ) );// phpcs:ignore WordPress.WP.I18n.NonSingularStringLiteralText
 
 				if ( is_multisite() ) {
-					esc_html_e( ' This will be applicable for all sites from the network.', 'all-in-one-schemaorg-rich-snippets' );
+					esc_html_e( ' This will be applicable for all sites from the network.' );
 				}
 				?>
 			</label>
 			<?php
-			echo wp_kses_post( sprintf( '<a href="%1s" target="_blank" rel="noreferrer noopener">%2s</a>', esc_url( $args['usage_doc_link'] ), __( 'Learn More.', 'all-in-one-schemaorg-rich-snippets' ) ) );
+			echo wp_kses_post( sprintf( '<a href="%1s" target="_blank" rel="noreferrer noopener">%2s</a>', esc_url( $args['usage_doc_link'] ), __( 'Learn More.' ) ) );
 			?>
 			</fieldset>
 			<?php
@@ -418,7 +446,7 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 		 * Set analytics installed time in option.
 		 *
 		 * @param string $source source of analytics.
-		 * @return string|mixed $time analytics installed time.
+		 * @return string $time analytics installed time.
 		 * @since 1.0.0
 		 */
 		private function get_analytics_install_time( $source ) {
@@ -483,22 +511,14 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 		/**
 		 * Save analytics option to network.
 		 *
-		 * @param string $option Name of option.
-		 * @param string $value Value of option.
+		 * @param string $option name of option.
+		 * @param string $value value of option.
 		 * @since 1.0.0
 		 */
 		public function add_option_to_network( $option, $value ) {
 
-			// Verify nonce to prevent CSRF attacks.
-			if ( ! isset( $_POST['_wpnonce'] ) || ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['_wpnonce'] ) ), 'general-options' ) ) {
-				return; // Stop execution if nonce is invalid.
-			}
-
-			// If action is coming from the general settings page.
-			if ( isset( $_POST['option_page'] ) && 'general' === sanitize_text_field( wp_unslash( $_POST['option_page'] ) ) ) {
-
-				$option = sanitize_key( $option ); // Sanitize option name.
-				$value  = sanitize_text_field( $value ); // Sanitize option value (use a different function if storing more complex data).
+			// If action coming from general settings page.
+			if ( isset( $_POST['option_page'] ) && 'general' === $_POST['option_page'] ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
 
 				if ( get_site_option( $option ) ) {
 					update_site_option( $option, $value );
@@ -508,5 +528,48 @@ if ( ! class_exists( 'BSF_Analytics' ) ) {
 			}
 		}
 
+		/**
+		 * Function to load the deactivation survey form on the admin footer.
+		 *
+		 * This function checks if the Deactivation_Survey_Feedback class exists and if so, it loads the deactivation survey form.
+		 * The form is configured with specific settings for plugin. Example: For CartFlows, including the source, logo, plugin slug, title, support URL, description, and the screen on which to show the form.
+		 *
+		 * @since 1.1.6
+		 * @return void
+		 */
+		public function load_deactivation_survey_form() {
+
+			if ( class_exists( 'Deactivation_Survey_Feedback' ) ) {
+				foreach ( $this->entities as $key => $data ) {
+					// If the deactibation_survery info in available then only add the form.
+					if ( ! empty( $data['deactivation_survey'] ) && is_array( $data['deactivation_survey'] ) ) {
+						foreach ( $data['deactivation_survey'] as $key => $survey_args ) {
+							Deactivation_Survey_Feedback::show_feedback_form(
+								$survey_args
+							);
+						}
+					}
+				}
+			}
+		}
+
+		/**
+		 * Function to add plugin slugs to Deactivation Survey vars for JS operations.
+		 *
+		 * @param array $vars UDS vars array.
+		 * @return array Modified UDS vars array with plugin slugs.
+		 * @since 1.1.6
+		 */
+		public function add_slugs_to_uds_vars( $vars ) {
+			foreach ( $this->entities as $key => $data ) {
+				if ( ! empty( $data['deactivation_survey'] ) && is_array( $data['deactivation_survey'] ) ) {
+					foreach ( $data['deactivation_survey'] as $key => $survey_args ) {
+						$vars['_plugin_slug'] = isset( $vars['_plugin_slug'] ) ? array_merge( $vars['_plugin_slug'], array( $survey_args['plugin_slug'] ) ) : array( $survey_args['plugin_slug'] );
+					}
+				}
+			}
+
+			return $vars;
+		}
 	}
 }
