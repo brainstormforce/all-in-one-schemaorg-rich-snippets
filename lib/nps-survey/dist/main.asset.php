6ebc34b0d8a12a0d668fbad657d4a9a4448e819a
Akshay Urankar <akshayurankar@Akshays-Laptop.local>
2025-07-23 06:31:24 +0530

    added necessay packages

--- lib/nps-survey/dist/main.asset.php
+++ lib/nps-survey/dist/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '098e9bb53b4690172575');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '9a83e54041811ee7b09c');
